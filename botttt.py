b23ca3473a5feb32b7ab0ac3b24275f9db5af4f9
kendaloi <nameeksss@gmail.com>
2026-07-16 13:08:13 +0300

    Update botttt.py

--- botttt.py
+++ botttt.py
@@ -74,5 +74,5 @@ def wh():
 def h(): return "Bot is running",200
 if __name__=="__main__":
     bot.remove_webhook()
-    bot.set_webhook(url=f"https://kenda-bot-elsz.onrender.com{TOKEN}")
+    bot.set_webhook(url=f"https://kenda-bot-elsz.onrender.com/{TOKEN}")
     app.run(host="0.0.0.0",port=int(os.environ.get("PORT",10000)))
